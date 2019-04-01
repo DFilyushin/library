7c8ea9e78efa1906768569723d62aebccadc50cf
D. Filyushin <dmitriy.filyushin@yandex.ru>
2019-04-01 21:22:16 +0700

    add settings

---
+++ dev_settings.py
@@ -0,0 +1,21 @@
+"""
+Librusec library settings
+"""
+
+# -*- coding: utf-8 -*-
+
+
+LIB_INDEXES = r'/home/filka/librusec/index'
+LIB_ARCHIVE = r'~\librusec'
+TMP_DIR = r'\tmp'
+
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+MONGO_DATABASE = "books"
+
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 0
+TASK_QUEUE_NAME = "btasks"
+
