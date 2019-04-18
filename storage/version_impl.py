fb9bdf3c2ba47f110b897c62f01876ea9d9803fc
DFilyushin <dmitriy.filyushin@ya.ru>
2019-04-18 14:11:28 +0700

    add version class

---
+++ storage/version_impl.py
@@ -0,0 +1,33 @@
+from pymongo.collection import Collection
+from pymongo.database import Database
+from storage.version import LibraryVersion, LibraryVersionDAO
+
+
+class MongoVersionDAO(LibraryVersionDAO):
+
+    def __init__(self, mongo_database: Database):
+        self.database = mongo_database
+
+    def create(self, version: LibraryVersion):
+        version = self.collection.insert_one(self.to_bson(version))
+        return version
+
+    @property
+    def collection(self) -> Collection:
+        return self.database['lversion']
+
+    @classmethod
+    def to_bson(cls, version: LibraryVersion):
+        result = {
+            k: v
+            for k, v in version.__dict__.items() if v is not None
+        }
+        return result
+
+    @classmethod
+    def from_bson(cls, document) -> LibraryVersion:
+        document['id'] = str(document.pop('_id'))
+        return LibraryVersion(**document)
+
+    def get_version(self):
+        return self.from_bson(self.collection.find_one())
