ed0d29712c30b9cdee72401f7332b12c03ee9970
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2019-04-20 13:37:01 +0700

    fix NewGenreLoader for load genres titles to object, not to list

--- tools/loader.py
+++ tools/loader.py
@@ -172,19 +172,21 @@ class NewGenreLoader(object):
             item = dict()
             item['parent'] = parent_name
             item['name'] = subgenre.get('value')
-            item['title'] = []
-            item['detailed'] = []
+            item['title'] = dict()
+            item['detailed'] = dict()
             for sg_item in subgenre.getchildren():
                 if sg_item.tag == 'genre-descr':
-                    item['title'].append({sg_item.get('lang'): sg_item.get('title')})
+                    lang = sg_item.get('lang')
+                    lang_title = sg_item.get('title')
+                    item['title'][lang] = lang_title
                 elif sg_item.tag == 'genre-alt':
                     item2 = dict()
                     item2['parent'] = parent_name
                     item2['name'] = sg_item.get('value')
                     item2['title'] = item['title']
-                    item2['detailed'] = []
+                    item2['detailed'] = dict()
                     if not self.genres.get(sg_item.get('value')):
-                        self.genres[sg_item.get('value')]=item2
+                        self.genres[sg_item.get('value')] = item2
                     else:
                         print('{} skipped 1'.format(sg_item.get('value')))
             if not self.genres.get(item['name']):
@@ -199,14 +201,16 @@ class NewGenreLoader(object):
             genre_item = dict()
             genre_item['name'] = appt.get('value')
             genre_item['parent'] = ''
-            genre_item['title'] = []
-            genre_item['detailed'] = []
+            genre_item['title'] = dict()
+            genre_item['detailed'] = dict()
             for elem in appt.getchildren():
                 if elem.tag == 'subgenres':
                     self.get_subgenre(elem, genre_item['name'])
                 elif elem.tag == 'root-descr':
-                    genre_item['title'].append({elem.get('lang'): elem.get('genre-title')})
-                    genre_item['detailed'].append({elem.get('lang'): elem.get('detailed')})
+                    lang = elem.get('lang')
+                    lang_title = elem.get('genre-title')
+                    genre_item['title'][lang] = lang_title
+                    genre_item['detailed'][elem.get('lang')] = elem.get('detailed')
             self.genres[appt.get('value')] = genre_item
 
         for key, item in self.genres.items():
@@ -233,22 +237,3 @@ def update_version():
         added=datetime.now().strftime('%Y%m%d')
     )
     wiring.library_dao.create(version)
-
-
-if __name__ == '__main__':
-
-    lang_loader = LanguageLoader()
-    lang_loader.process()
-
-    exit()
-
-    book_loader = BookLoader()
-    book_loader.process()
-
-    gl = GenreLoader()
-    gl.process()
-
-    new_genre = NewGenreLoader()
-    new_genre.process()
-
-    update_version()
